8c58f84a3492f09dfc5cfb4ddf6ce60a9e0cc3ee
mishab <mishab.ak@pardypanda.com>
2024-09-04 15:50:04 +0530

    add sunset

--- controller/liveWeather.js
+++ controller/liveWeather.js
@@ -1,6 +1,6 @@
 const { default: axios } = require("axios");
 const moment = require("moment");
-const {saveWeather} = require("./mongod");
+const { saveWeather } = require("./mongod");
 
 const liveWeather = async (lat, lon) => {
   try {
@@ -10,19 +10,15 @@ const liveWeather = async (lat, lon) => {
 
     let startDate = moment().startOf("hours").format("X");
     let endDate = moment().startOf("hours").add(10, "hours").format("X");
+    let foreCast = res.data?.forecast?.forecastday[0] || {};
+    let obj = { hour: [] };
 
-    let obj = { current: {}, hour: [], location: {} };
-
-    if (res?.data?.location) {
-      obj.location = res.data?.location;
-    }
-      
-    if (res?.data?.current) {
-      obj.current = res.data?.current;
-    } 
+    obj.location = res.data?.location || {};
+    obj.current = res.data?.current || {};
+    obj.sunset = foreCast?.astro?.sunset || "";
 
     if (res.data?.forecast && res.data?.forecast?.forecastday) {
-      obj.hour = [...res.data?.forecast?.forecastday[0].hour]
+      obj.hour = [...res.data?.forecast?.forecastday[0].hour];
       obj.hour = obj.hour.filter(
         (e) => e.time_epoch >= startDate && e.time_epoch <= endDate
       );
